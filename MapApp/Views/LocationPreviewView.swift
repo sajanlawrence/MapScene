fcdcb46e3244bbcc43e59fae8a88197fbdc3c2b7
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-09-11 00:12:22 +0530

    Created LocationPreview box

---
+++ MapApp/Views/LocationPreviewView.swift
@@ -0,0 +1,93 @@
+//
+//  LocationPreviewView.swift
+//  MapApp
+//
+//  Created by Sajan Lawrence on 10/09/25.
+//
+
+import SwiftUI
+
+struct LocationPreviewView: View {
+    @State var location: Location
+    var body: some View {
+        HStack(alignment: .bottom, spacing: 0.0) {
+            VStack(alignment: .leading){
+                imageSection
+                titleSection
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack(spacing: 8.0){
+                learnMoreButton
+                nextButton
+            }
+        }
+        .padding(20)
+        .background {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.ultraThinMaterial)
+                .offset(y: 65)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+        
+    }
+}
+
+extension LocationPreviewView{
+    private var imageSection: some View{
+        ZStack{
+            if let imageName = location.imageNames.first{
+                Image(imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+            }
+        }
+        .padding(8)
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+    }
+    
+    private var titleSection: some View{
+        VStack(alignment: .leading){
+            Text(location.name)
+                .font(.title2)
+                .bold()
+            Text(location.cityName)
+                .font(.subheadline)
+        }
+    }
+    
+    private var learnMoreButton: some View{
+        Button {
+            
+        } label: {
+            Text("Learn more")
+                .frame(width: 100, height: 30)
+            
+        }
+        .buttonStyle(.borderedProminent)
+    }
+    
+    private var nextButton: some View{
+        Button {
+            
+        } label: {
+            Text("Next")
+                .frame(width: 100, height: 30)
+            
+        }
+        .buttonStyle(.bordered)
+    }
+}
+
+#Preview {
+    ZStack{
+        Color.gray
+            .ignoresSafeArea()
+        LocationPreviewView(location: Location.default)
+            .padding()
+    }
+        
+}
